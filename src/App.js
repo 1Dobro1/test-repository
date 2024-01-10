1e606ab40b32ddc6e86a96cfd141101a908bd4a9
1Dobro1 <kushnarev.02@list.ru>
2024-01-10 18:33:57 +0300

    feat: Added semantic-release

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Шевченко С.В.";
+  const greeting = "Это тестовый проект Шевченко С.В.
 
-
+  // console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
